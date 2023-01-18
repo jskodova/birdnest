df3dd9d6e05dadcf1ae98343668231bedc72d751
jskodova <skodovajarmilatt@gmail.com>
2023-01-18 23:32:11 +0200

    replaced mysql with postgres

--- src/main/java/com/birdnest/app/Main.java
+++ src/main/java/com/birdnest/app/Main.java
@@ -54,11 +54,10 @@ public class Main {
 
                     //Save Drone objects to database
                     //Create a connection to the database first
-                    try (Connection conn = DriverManager.getConnection(
-                            "jdbc:mysql://sql11.freesqldatabase.com:3306/sql11591609?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
-                            "sql11591609", "kqw3Tl1thj");
-                         Statement stmt = conn.createStatement();
-                    ) {
+                    String connectionUrl = "jdbc:postgres://skodovajarmilatt:KvNA72zFsdey@ep-royal-paper-930875.eu-central-1.aws.neon.tech/neondb";
+                    String username = "skodovajarmilatt";
+                    String password = "KvNA72zFsdey";
+                    try (Connection connection = DriverManager.getConnection(connectionUrl, username, password)) {
                         String dropDB = "UPDATE drones SET expiration_time = DATE_ADD(NOW(), INTERVAL 10 MINUTE) WHERE expiration_time IS NULL OR expiration_time < NOW();";
                         //Create the database table if it doesn't exist
                         String createTableSQL = "CREATE TABLE IF NOT EXISTS drones (" +
@@ -73,28 +72,27 @@ public class Main {
                                 "phoneNumber VARCHAR(15)," +
                                 "email VARCHAR(50)" +
                                 ")";
-                        stmt.execute(dropDB);
-                        stmt.execute(createTableSQL);
-
-                        //Save each drone object to the database
                         for (Drone drone : dronesWithPilots) {
-                            String insertTableSQL = "INSERT INTO drones" +
-                                    "(serialNumber, manufacturer, model, positionX, positionY, pilotId, firstName, lastName, phoneNumber, email) " +
-                                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                            PreparedStatement pstmt = conn.prepareStatement(insertTableSQL);
-                            pstmt.setString(1, drone.getSerialNumber());
-                            pstmt.setString(2, drone.getManufacturer());
-                            pstmt.setString(3, drone.getModel());
-                            pstmt.setFloat(4, drone.getPositionX());
-                            pstmt.setFloat(5, drone.getPositionY());
-                            pstmt.setString(6, drone.getPilotId());
-                            pstmt.setString(7, drone.getFirstName());
-                            pstmt.setString(8, drone.getLastName());
-                            pstmt.setString(9, drone.getPhoneNumber());
-                            pstmt.setString(10, drone.getEmail());
-                            pstmt.executeUpdate();
-                        }
+                            String query = "INSERT INTO drones (serialNumber, manufacturer, model, positionX, positionY, pilotId, firstName, lastName, phoneNumber, email) VALUES ((?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                        PreparedStatement statement = connection.prepareStatement(query);
+                        PreparedStatement stmnt = connection.prepareStatement(dropDB);
+                        PreparedStatement create = connection.prepareStatement(createTableSQL);
+                        statement.setString(1, drone.getSerialNumber());
+                        statement.setString(2, drone.getManufacturer());
+                        statement.setString(3, drone.getModel());
+                        statement.setFloat(4, drone.getPositionX());
+                        statement.setFloat(5, drone.getPositionY());
+                        statement.setString(6, drone.getPilotId());
+                        statement.setString(7, drone.getFirstName());
+                        statement.setString(8, drone.getLastName());
+                        statement.setString(9, drone.getPhoneNumber());
+                        statement.setString(10, drone.getEmail());
+                        create.executeUpdate();
+                        statement.executeUpdate();
+                        stmnt.executeUpdate();
 
+                        ResultSet resultSet = statement.executeQuery("SELECT * FROM drones");
+                    }
                     } catch (SQLException ex) {
                         System.out.println(ex.getMessage());
                     }
